44e1e3eef101ac13ade11adef5de362e53e441fa
Louis <louis.foged@gmail.com>
2022-03-28 11:55:49 +0200

    chore: remove commented out code

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 // no try/catch. Crash on error at this level => handled in processErrorHandler
 const init = async () => {
     logger('LOG: connecting to DB');
-    // await connectDB();
+    await connectDB();
     logger('LOG: DB connected');
     
     app.listen(PORT, () => logger(`LOG: server running on port ${PORT}`));
